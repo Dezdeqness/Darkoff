1af173ece383ec0609a5c6b3f5cbfac53bc90f6c
Danyl Brahin <inicerer141@gmail.com>
2026-02-16 20:47:44 +0200

    [MVP] DI

--- lib/service_locator/service_locator.dart
+++ lib/service_locator/service_locator.dart
@@ -1,10 +1,13 @@
 import 'package:darkoff/core/config/app_config.dart';
 import 'package:darkoff/service_locator/categories_service_locator.dart';
 import 'package:darkoff/service_locator/graphql_service_locator.dart';
+import 'package:darkoff/service_locator/items_service_locator.dart';
 
 Future<void> setupServiceLocator() async {
   await AppConfig.initialize();
-  
-  setupCategoriesServiceLocator();
+
   await setupGraphQLServiceLocator();
+
+  setupCategoriesServiceLocator();
+  setupItemsServiceLocator();
 }
